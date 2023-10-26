c2dd2d2b5bb1ce120f247c86293bf29635c099eb
Pavel-K-ua <113534910+Pavel-K-ua@users.noreply.github.com>
2023-10-27 01:28:31 +0300

    add persistor

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from 'App.jsx';
 import 'modern-normalize/modern-normalize.css';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
-// import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <Provider store={store}>
       <BrowserRouter basename="/rent-a-car">
-        {/* <PersistGate loading={null} persistor={persistor}> */}
-        <App />
-        {/* </PersistGate> */}
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,36 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { rentACarReducer } from './slice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rentACarReducer);
 
 export const store = configureStore({
   reducer: {
-    rentACar: rentACarReducer,
+    rentACar: persistedReducer,
   },
-  //   devTools: process.env.NODE_ENV !== 'production',
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV !== 'production',
 });
+
+export const persistor = persistStore(store);
