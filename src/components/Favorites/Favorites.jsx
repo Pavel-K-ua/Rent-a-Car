b082f069c83d3591f11253a0bd12dba60386f91d
Pavel-K-ua <113534910+Pavel-K-ua@users.noreply.github.com>
2023-10-27 01:14:35 +0300

    add to fav page

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import Card from 'components/Card/Card';
+import { StyledCatalog } from 'components/Catalog/Catalog.styled';
+import { Modal } from 'components/Modal/Modal';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectFavorites } from 'redux/selectors';
 
 const Favorites = () => {
-  return <div>Favorites</div>;
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [currentCar, setCurrentCar] = useState();
+  const favorites = useSelector(selectFavorites);
+  const isFavorite = item => favorites.includes(item);
+  const handleOpenModal = car => {
+    setIsOpenModal(!isOpenModal);
+    setCurrentCar(car);
+  };
+  return (
+    <>
+      <div>
+        Favorites
+        <StyledCatalog>
+          {favorites.map(car => (
+            <Card key={car.id} car={car} handleOpenModal={handleOpenModal} />
+          ))}
+        </StyledCatalog>
+      </div>
+      {isOpenModal && <Modal close={handleOpenModal} car={currentCar}></Modal>}
+    </>
+  );
 };
 
 export default Favorites;
