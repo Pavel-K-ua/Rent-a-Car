a8eb4ee2772dba318345930c7a04c47d0a7864be
Pavel-K-ua <113534910+Pavel-K-ua@users.noreply.github.com>
2023-10-27 12:31:16 +0300

    base name fix

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <Provider store={store}>
-      <BrowserRouter basename="/rent-a-car">
+      <BrowserRouter basename="/Rent-a-Car">
         <PersistGate loading={null} persistor={persistor}>
           <App />
         </PersistGate>
